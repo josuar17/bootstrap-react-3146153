bb3f265866f73f712bc45a538a4339b2ba1879dd
josuar17 <josuaravila1707@gmail.com>
2025-11-18 13:24:05 -0500

    Avance metrika

---
+++ 03-Metrika/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { SectionCard } from './componentes/SectionCard.jsx'
+
+export const App = () => {
+  return (
+    <>
+    <div>App</div>
+    
+    <SectionCard/>
+    </>
+  )
+}
+
+
---
+++ 03-Metrika/src/componentes/SectionCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const SectionCard = () => {
+  return (
+    <div>
+        <h1>
+            Hola Alexa
+        </h1>
+    </div>
+  )
+}
+
